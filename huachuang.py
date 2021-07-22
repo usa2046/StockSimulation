9d50b3f8d01289546cce6693f592c2959c16ee42
tamray <985480276@qq.com>
2021-07-22 20:42:12 +0800

    保存

--- huachuang.py
+++ huachuang.py
@@ -17,6 +17,10 @@ import pywinauto.application
 from pywinauto.application import Application
 import time
 
+import cv2
+import numpy as np
+from PIL import ImageGrab
+
 def Color():
     print("\033[1;30m 字体颜色30m \033[0m")
     print("\033[1;31m 字体颜色31m \033[0m")
@@ -42,6 +46,8 @@ def printc(type, str):
         print("\033[1;31m%s\033[0m" % (str))
     elif type == -1: # 买
         print("\033[1;32m%s\033[0m" % (str))
+    elif type == 2: # except
+        print("\033[1;37m%s\033[0m" % (str))
     else:
         print("\033[1;38m%s\033[0m" % (str))
 
@@ -63,16 +69,36 @@ class HuaChuang():
             printc(0, "请先打开交易软件并登录！")
             exit(0)
 
-    def pop_up_window_confirmation(self):
-        '''弹出窗口中确认交易'''
-        top = pywinauto.findwindows.find_windows(top_level_only=True, class_name='#32770')[0]
-        top_win = self.app.window(handle=top)
-        top_win.Button0.click()
-        time.sleep(0.2)
-        top = pywinauto.findwindows.find_windows(top_level_only=True, class_name='#32770')[0]
-        top_win = self.app.window(handle=top)
-        top_win.Button0.click()
-        time.sleep(0.2)
+    def pop_up_window_confirmation(self, type=0):
+        '''弹出窗口中确认交易，有两次弹框
+        :param type: 0-买卖单弹出框 1-全撤单弹出框
+        '''
+
+        while True:
+            try:
+                top = pywinauto.findwindows.find_windows(top_level_only=True, class_name='#32770')[0]
+                top_win = self.app.window(handle=top)
+                time.sleep(0.1)
+                if type == 0:
+                    top_win.Button0.click()
+                else:
+                    top_win.Button3.click()
+                printc(0, ">> Pop up window confirmation 1")
+                break
+            except:
+                printc(2, "except: Pop up window confirmation 1")
+                time.sleep(0.5)
+        while True:
+            try:
+                top = pywinauto.findwindows.find_windows(top_level_only=True, class_name='#32770')[0]
+                top_win = self.app.window(handle=top)
+                time.sleep(0.1)
+                top_win.Button0.click()
+                printc(0, ">> Pop up window confirmation 2")
+                break
+            except:
+                printc(2, "excpet: Pop up window confirmation 2")
+                time.sleep(0.5)
 
     def buy(self, code, price, num):
         '''买入
@@ -81,15 +107,22 @@ class HuaChuang():
         :param num:   股票数量
         '''
         self.tree_win.get_item(r"\买入[F1]").click()
-        time.sleep(0.2)
-        self.dialog_hwnd = \
-            pywinauto.findwindows.find_windows(top_level_only=False, class_name='#32770', parent=self.top_hwnd)[0]
-        self.dialog_win = self.app.window(handle=self.dialog_hwnd)
-        self.dialog_win.Edit1.set_edit_text(code)
-        self.dialog_win.Edit2.set_edit_text(price)
-        self.dialog_win.Edit3.set_edit_text(num)
-        self.dialog_win.Button1.click()
-        time.sleep(0.2)
+        while True:
+            try:
+                self.dialog_hwnd = pywinauto.findwindows.find_windows(top_level_only=False,
+                                                                      class_name='#32770',
+                                                                      parent=self.top_hwnd)[0]
+                self.dialog_win = self.app.window(handle=self.dialog_hwnd)
+                self.dialog_win.Edit1.set_edit_text(code)
+                time.sleep(0.1) # 价格有初始值，延迟长点避免叠加
+                self.dialog_win.Edit2.set_edit_text(price)
+                self.dialog_win.Edit3.set_edit_text(num)
+                self.dialog_win.Button1.click()
+                printc(0, ">> Press 'buy' button")
+                break
+            except:
+                printc(2, "except: Press 'buy' button")
+                time.sleep(0.5)
         self.pop_up_window_confirmation()
         printc(-1, "[买入委托] 代码：%8s 价格：%8s 数量：%8s 市值：%d" %
                (code.ljust(8), price.ljust(8), num.ljust(8), float(price)*int(num)))
@@ -101,23 +134,64 @@ class HuaChuang():
         :param num:   股票数量
         '''
         self.tree_win.get_item(r"\卖出[F2]").click()
-        time.sleep(0.2)
-        self.dialog_hwnd = \
-            pywinauto.findwindows.find_windows(top_level_only=False, class_name='#32770', parent=self.top_hwnd)[0]
-        self.dialog_win = self.app.window(handle=self.dialog_hwnd)
-        self.dialog_win.Edit1.set_edit_text(code)
-        self.dialog_win.Edit2.set_edit_text(price)
-        self.dialog_win.Edit3.set_edit_text(num)
-        self.dialog_win.Button1.click()
-        time.sleep(0.2)
+        while True:
+            try:
+                self.dialog_hwnd = \
+                    pywinauto.findwindows.find_windows(top_level_only=False, class_name='#32770', parent=self.top_hwnd)[0]
+                self.dialog_win = self.app.window(handle=self.dialog_hwnd)
+                self.dialog_win.Edit1.set_edit_text(code)
+                time.sleep(0.1) # 价格有初始值，延迟长点避免叠加
+                self.dialog_win.Edit2.set_edit_text(price)
+                self.dialog_win.Edit3.set_edit_text(num)
+                self.dialog_win.Button1.click()
+                printc(0, ">> Press 'sell' button")
+                break
+            except:
+                printc(2, "except: Press 'sell' button")
+                time.sleep(0.5)
         self.pop_up_window_confirmation()
         printc(1, "[卖出委托] 代码：%8s 价格：%8s 数量：%8s 市值：%d" %
                (code.ljust(8), price.ljust(8), num.ljust(8), float(price)*int(num)))
 
+    def cancellations(self):
+        '''全撤
+        '''
+        self.dialog_hwnd = \
+            pywinauto.findwindows.find_windows(top_level_only=False, class_name='#32770', parent=self.top_hwnd)[0]
+        self.dialog_win = self.app.window(handle=self.dialog_hwnd)
+        time.sleep(0.1)  # 价格有初始值，延迟长点避免叠加
+        self.dialog_win.Button4.click()
+        self.pop_up_window_confirmation(type=1)
+        printc(1, "[委托全撤] 所有委托单已撤销")
+
+    def exception_dell(self):
+        '''异常处理，关闭所有弹出窗口，检查数据，恢复异常'''
+        pass
+
+    def read_stock(self):
+        '''读取持有股票信息
+        '''
+        self.dialog_win.CVirtualGridCtrl.right_click(coords=(30, 30))
+        self.main_win.type_keys('C')
+        data = pywinauto.clipboard.GetData()
+        print(data)
+        #self.stock_hwnd = \
+        #    pywinauto.findwindows.find_windows(top_level_only=False, class_name='CVirtualGridCtrl', parent=self.top_hwnd)[0]
+        #self.stock_win = self.app.window(handle=self.stock_hwnd)
+        #print(self.stock_win.print_control_identifiers())
 
 if __name__ == "__main__":
     huachuang = HuaChuang()
-    huachuang.buy("000027", "9.54", "100")
-    #huachuang.sell("002497", "26.7", "100")
+    #huachuang.buy("000027", "9.54", "100")
+    #huachuang.sell("002497", "24.9", "100")
+    #huachuang.cancellations()
+    huachuang.read_stock()
+    #img = ImageGrab.grab(bbox=(0, 0, 100, 100))
+    #img = np.array(img.getdata(), np.uint8).reshape(img.size[1], img.size[0], 3)
+    #cv2.imshow("1", img)
+    #cv2.waitKey(0)
+
+
+
 
 
